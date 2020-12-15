4ee60d6b06a59d0c6cf10d1e58139e46bfb89353
Jake Fireside <labountypaul@icloud.com>
2020-12-15 17:57:38 -0600

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 09_Employee_Tracker
+
+
+Create a command line application that allows the user to see a list of departments, roles, and employees as well as be able to create new departments, role, and employees to eat section and them show them a list of each section to see a up to date list of each.
--- app.js
+++ app.js
@@ -17,7 +17,52 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
+    start() 
   });
+
+  function start() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+          "Add a department",
+          "Add a role",
+          "Add a employee",
+          "View departments",
+          "View roles",
+          "View employees"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "Add a department":
+          addDepartment();
+          break;
+  
+        case "Add a role":
+          addRole();
+          break;
+  
+        case "Add a employee":
+          addEmployee();
+          break;
+  
+        case "View departments":
+          allDepartments();
+          break;
+  
+        case "View roles":
+          allRoles();
+          break;
+
+         case "View employees":
+           allEmployees()
+
+        }
+      });
+  }
 
 
   // would be in a inquirer function
@@ -42,12 +87,66 @@ connection.connect(function(err) {
     ])
   
   .then(function(answers){
-    connection.query('INSERT INTO role SET ?', {title: answers.title,
+    connection.query('INSERT INTO role SET ?', {
+        title: answers.title,
         salary: answers.salary, 
         department_id: answers.department_id}, function(err){
          console.log('Success');
+         start()
      })
   })
+}
+
+function addEmployee(){
+  inquirer
+  .prompt([{
+      name: 'first_name',
+      type: 'input',
+      message: 'Employee first name?'
+  },
+  {
+      name: 'last_name',
+      type: 'input',
+      message: 'Employee last name?'
+  },
+  {
+      name: 'role_id',
+      type: 'input',
+      message: 'Role number?'
+  }
+      
+  ])
+
+.then(function(answers){
+  connection.query('INSERT INTO employee SET ?', {
+      first_name: answers.first_name,
+      last_name: answers.last_name, 
+      role_id: answers.role_id}, function(err){
+      if (err) console.error(err)
+       console.log('Success');
+       start()
+   })
+})
+}
+
+function addDepartment(){
+  inquirer
+  .prompt([{
+      name: 'department',
+      type: 'input',
+      message: 'Department name?'
+  },
+    
+  ])
+
+.then(function(answers){
+  connection.query('INSERT INTO department SET ?', {
+      Name: answers.department
+     }, function(err){
+       console.log('Success');
+       start()
+   })
+})
 }
 
 // addRole()
@@ -56,19 +155,26 @@ connection.connect(function(err) {
   // make these functions that call be called when needed.
 
 
+function allEmployees() {
+  connection.query("SELECT * FROM employee", function(err, res) {
+      console.table(res)
+      start()
+  });
+}
 
-//   connection.query("SELECT * FROM employee", function(err, res) {
-//       console.table(res)
-//   });
-function allRole()
+function allRoles() {
   connection.query("SELECT * FROM role", function(err, res) {
       console.table(res)
-  });
+      start()
+  })
+};
 
-  function allDepartments()
-//   connection.query("SELECT * FROM deparment", function(err, res) {
-//       console.table(res)
-//   });
+  function allDepartments() {
+  connection.query("SELECT * FROM department", function(err, res) {
+      console.table(res)
+      start()
+  });
+}
 
 
 
